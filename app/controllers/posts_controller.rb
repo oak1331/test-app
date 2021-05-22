ee2ec0b7b4aa0ea1ce8ff21bf62b266c88fac61f
oak1331 <oaks1331@gmail.com>
2021-05-22 16:28:19 +0900

    new及びcreateアクションの定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,12 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(memo: params[:memo])
+  end
 end
