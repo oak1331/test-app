fe6091fa11f2e46ad6b36677760f20b1d0c45ad8
oak1331 <oaks1331@gmail.com>
2021-05-22 18:22:03 +0900

    Userモデルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_091745) do
+ActiveRecord::Schema.define(version: 2021_05_22_092112) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "memo"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_05_22_091745) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
