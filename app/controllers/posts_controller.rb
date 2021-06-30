939cf47e1ec9987708fec29fc5f57acf7a6caa80
oak1331 <oaks1331@gmail.com>
2021-07-01 04:27:23 +0900

    アクションの定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:edit, :update, :show, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -9,10 +13,42 @@ class PostsController < ApplicationController
 
   def create
     Post.create(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:memo)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless @post.user.id == current_user.id
   end
 end
