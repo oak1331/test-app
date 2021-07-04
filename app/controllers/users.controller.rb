1f41da20d26535d759ce7bbdbbf97ea45f6b7bff
oak1331 <oaks1331@gmail.com>
2021-07-05 05:35:58 +0900

    editアクションの定義

---
+++ app/controllers/users.controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update, :show]
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :move_to_index, only: [:edit, :update]
+
+  def edit
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+  def move_to_index
+    redirect_to root_path unless @user.id == current_user.id
+  end
+end
