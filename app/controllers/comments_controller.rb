7fba24094446b771fb9fff475e65e3347197996d
oak1331 <oaks1331@gmail.com>
2021-07-10 15:08:33 +0900

    create及びdestroyアクションの定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,25 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@comment.post)
+    else
+      @post = @comment.post
+      @comments = @post.comments.includes(:user)
+      render "posts/show"
+    end
+  end
+
+  def destroy
+    comment = Comment.find_by(id: params[:id], post_id: params[:post_id])
+    comment.destroy
+    redirect_to post_path(comment.post)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
 end
