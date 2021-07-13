7ff70aec86155620a825d111360cbbd90650b2ad
oak1331 <oaks1331@gmail.com>
2021-07-14 05:13:59 +0900

    ラベルのアイコン化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.all.page(params[:page])
+    @posts = Post.all.page(params[:page]).per(10)
   end
 
   def new
