c6e95510247611f484f81ed948040c83fba0995e
oak1331 <oaks1331@gmail.com>
2021-07-27 05:46:38 +0900

    アソシエーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_one_attached :image
+  has_many_attached :image
 
   with_options presence: true do
     validates :memo
