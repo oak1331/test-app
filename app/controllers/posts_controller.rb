48f49285bc8a461ffc9ab27ae3abef79188ab88f
oak1331 <oaks1331@gmail.com>
2021-09-01 20:24:41 +0900

    ビューの編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :search, :latest, :popular]
   before_action :set_post, only: [:edit, :update, :show, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
